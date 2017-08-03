7a629218a12caa414a7243521f148c300b0fa7b7
mary <maria.veghova@eea.sk>
2017-08-03 15:32:50 +0200

    changes

--- src/action/FirstAction.java
+++ src/action/FirstAction.java
@@ -2,15 +2,41 @@ package action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.project.Project;
 
-public class FirstAction extends AnAction{
+import java.io.IOException;
 
-    public FirstAction(){
+
+public class FirstAction extends AnAction {
+
+    public FirstAction() {
         super("My First Action");
     }
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        System.out.println("My first action performed.");
+        System.out.println("Action performed.");
+
+        String basePath = getProjectBasePath(anActionEvent);
+        System.out.println(basePath);
+
+        executeGitCommand("");
+    }
+
+
+    private String getProjectBasePath(AnActionEvent anActionEvent) {
+        Project project = (Project) anActionEvent.getDataContext().getData(DataConstants.PROJECT);
+        return project.getBasePath();
+    }
+
+    private void executeGitCommand(String fileName) {
+        try {
+            Process p = Runtime.getRuntime().exec("cmd /c && dir > D:\\log.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
+
